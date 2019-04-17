92b63a07b5c865d1c296d4e0ed6376d15c706b08
djeanner <djeanner@users.noreply.github.com>
2019-04-17 19:56:02 +0200

    update for test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,19 +1,18 @@
-// I had to comment everything! with /* */
-/*
+
 // import { NMReDATA } from '..';
 
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
 
-
+/*
 test('Test call all NMRrecords', () => {
 // here a loop over the zip files in the ../testFiles folder
 // verify not generate eror when callin nmr_record.js
   let this_NMRrecord = nmr_record(...)
   expect();
-}
+}*/
 describe('test myModule', () => {
   it('should return 42', () => {
     expect(true).toBe(true);
   });
 });
-*/
+
