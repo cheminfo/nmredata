69a770a366472d0fdee0bbe4a1d87a4de395d3e5
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-06-28 17:20:38 +0200

    minor fixes

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
-const {readFileSync, writeFileSync} = require('fs');
-const readNmrRecord = require('./src/index');
+const { readFileSync, writeFileSync } = require('fs');
+
 const { resolve } = require('path');
-console.log(readNmrRecord)
+const readNmrRecord = require('./src/index');
+
+console.log(readNmrRecord);
 var zipData = readFileSync(resolve('testFiles/menthol_1D_1H_assigned_J.zip'), 'base64');
 
-readNmrRecord(zipData).then(result => {
-    console.log(result);
-    writeFileSync('nmredataToElnSample.json',JSON.stringify(result));
-})
-// fs.appendFileSync('data.json', JSON.stringify(toExport) + ',');   
+readNmrRecord(zipData).then((result) => {
+  console.log(result);
+  writeFileSync('nmredataToElnSample.json', JSON.stringify(result));
+});
+// fs.appendFileSync('data.json', JSON.stringify(toExport) + ',');
