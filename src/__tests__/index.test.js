0a3fb8207642e81b95e5132fbe335909f63aa1c8
djeanner <djeanner@users.noreply.github.com>
2019-06-29 07:59:35 +0200

    minor fixes eslint

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,10 @@
 import { readFileSync } from 'fs';
-import { nmredata } from '../index';
 import { resolve } from 'path';
-console.log(nmredata)
+
+import { nmredata } from '../index';
+
+
+console.log(nmredata);
 describe('test myModule', () => {
   it('should return 42', () => {
     var zipData = readFileSync(resolve('testFiles/menthol_1D_1H_assigned_J.zip'), 'base64');
