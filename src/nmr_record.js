0b1d88e2c4a9ea5a80657af6c1ebb3af33f6a7a6
jobo322 <jose.bolanos@correounivalle.edu.co>
2019-09-06 07:04:47 -0500

    fix constructor and implement getNMReDATATags

--- src/nmr_record.js
+++ src/nmr_record.js
@@ -1,8 +1,74 @@
-import {getNMReDATAtags} from './';
-import {getNMReDATA} from './';
-import * as Jszip from 'jszip';
+// import {getNMReDATAtags} from './'; //to delete
+// import {getNMReDATA} from './'; //to delete
+import jszip from 'jszip';
+import {parse} from './parser/parseSDF';
 import * as OCLfull from 'openchemlib-extended';
+// import * as getNMReData from 'getNMReData';
 
+export class nmrRecord {
+  constructor(nmrRecord) {
+    if (!nmrRecord instanceof Object) {
+      throw new Error('Cannot be called directly');
+    }
+    let {folders, sdfFiles} = nmrRecord
+    this.folders = folders;
+    this.sdfFiles = sdfFiles;
+    this.activeElement = 0;
+    return this;
+  }
+
+  static async fromZipFile(nmrRecord) {
+    let data = await readNmrRecord(nmrRecord)
+    return new this(data);
+  }
+
+  getMol(i = this.activeElement) {
+    let parserResult = this.sdfFiles[i];
+    return parserResult.molecules[0].molfile;
+  }
+
+  getMoleculeAndMap(i = this.activeElement) {
+    let molfile = this.getMol(i);
+    return OCLfull.Molecule.fromMolfileWithAtomMap(molfile);
+  }
+
+  getNMReDATAtags(i = this.activeElement) {
+    let nmredataTags = {};
+    let sdfFile = this.sdfFiles[i];
+    let version = parseFloat(sdfFile.molecules[0]['NMREDATA_VERSION']);
+    let toReplace = version > 1 ? [new RegExp(/\\\n*/g), '\n'] : []
+    // Object.keys(sdfFile.molecules[0]).map((tag) => { //should give the same result that the next line
+    sdfFile.labels.forEach((tag) => {
+      if (tag.toLowerCase().match('nmredata')) {
+        let key = tag.replace(/NMREDATA\_/, '')
+        let data = sdfFile.molecules[0][tag].replace(toReplace[0], toReplace[1]);
+        nmredataTags[key] = data;
+      }
+    });
+    return nmredataTags;
+  }
+
+  // getNMReData(i = this.activeElement) {
+  //   let nmredataTags =  this.getNMReDATAtags(i);
+  //   //not implemented
+  // }
+
+  getAllTags(i = this.activeElement) {
+    let allTags = {};
+    let sdfFile = this.sdfFiles[i];
+    // Object.keys(sdfFile.molecules[0]).map((tag) => { //should give the same result that the next line
+    sdfFile.labels.forEach((tag) => {
+      if (tag.toLowerCase() !== 'molfile') {
+        allTags[tag] = sdfFile.molecules[0][tag];
+      }
+    })
+    return allTags;
+  }
+
+  toJSON(i = this.activeElement) {
+    
+  }
+}
 
 /**
  * Read nmr record file asynchronously
@@ -11,9 +77,9 @@ import * as OCLfull from 'openchemlib-extended';
  * @return {} An Object with two properties folders and sdfFiles, folders has nmr spectra data, sdfFiles has all sdf files
  */
 async function readNmrRecord(zipData, options = {}) {
-  var zip = new Jszip();
+  var zip = new jszip();
   return zip.loadAsync(zipData, {base64: true}).then(async (zipFiles) => {
-    let sdfFiles = getSDF(zipFiles, options);;
+    let sdfFiles = await getSDF(zipFiles, options);;
     var folders = zipFiles.filter(function (relativePath, file) {
         if(relativePath.indexOf("ser")>=0||relativePath.indexOf("fid")>=0
             ||relativePath.indexOf("1r")>=0||relativePath.indexOf("2rr")>=0) {
@@ -31,7 +97,7 @@ async function readNmrRecord(zipData, options = {}) {
  * @param {*} options 
  * @returns {Array} Array of sdf parsed files
  */
-function getSDF(zipFiles, options = {}) {
+async function getSDF(zipFiles, options = {}) {
   let result = [];
   for (let file in zipFiles.files) {
       let pathFile = file.split('/');
@@ -44,56 +110,13 @@ function getSDF(zipFiles, options = {}) {
           }
           let sdf = await zipFiles.file(file).async('string');
           // sdf = sdf.replace(/\\/g, '')
+          // console.log(parse)
           let parserResult = parse(sdf + '', {mixedEOL: true});
           result.push(parserResult);
       }
   }
   return result;
 }
-export class nmrRecord {
-
-  constructor(nmrRecord) {
-    let data = await readNmrRecord(nmrRecord)
-    this.folders = data.folders;
-    this.sdfFiles = data.sdfFiles;
-    this.activeElement = 0;
-  }
-
-  getMol(i = this.activeElement) {
-    let parserResult = this.sdfFiles[i];
-    return parserResult.molecules[0].molfile;
-  }
-
-  getMoleculeAndMap(i = this.activeElement) {
-    let molfile = this.getMol(i);
-    return OCLfull.Molecule.fromMolfileWithAtomMap(molfile);
-  }
-
-  toJSON(i = this.activeElement) {
-    
-  }
-
-  getNMReDATAtags(i = this.activeElement) {
-    let nmredataTags = [];
-    let sdfFile = this.sdfFiles[i];
-    Object.keys(sdfFile).map((tag) => {
-      if (tag.toLowerCase().match('nmredata')) {
-        nmredataTags.push(sdfFile.molecules[0][tag])
-      }
-    })
-    return nmredataTags;
-  }
-
-  getAllTags(i = this.activeElement) {
-    let allTags = [];
-    let sdfFile = this.sdfFiles[i];
-    Object.keys(sdfFile).map((tag) => {
-      if (tag.toLowerCase() !== 'molfile') {
-        allTags.push(sdfFile.molecules[0][tag])
-      }
-    })
-    return allTags;
-  }
   // here: loop testing all .sdf files in the zip_object 
   // As an option ig should explore subfolders
   // When the zip is an NMReDATA record, it should find the file
@@ -110,34 +133,34 @@ export class nmrRecord {
   // .sdf files may include multiple structures... each has has its assiciated tags...
   // loop over structures in a given .sdf file. We may have two when there is a flat and a 3D structures...
 
-let molblock = currentSDFfile.getmol(loop);// replace with  existing modults to get molblock...
-let all_tags = currentSDFfile.getNMReDATAtags(loop);// replace with existing module to read SDF tags....
-let nmredata_tags = all_tags.getNMReDATAtags();// just keep the tags including "NMEDATA in the tag name"
+// let molblock = currentSDFfile.getmol(loop);// replace with  existing modults to get molblock...
+// let all_tags = currentSDFfile.getNMReDATAtags(loop);// replace with existing module to read SDF tags....
+// let nmredata_tags = all_tags.getNMReDATAtags();// just keep the tags including "NMEDATA in the tag name"
   //maybe it is faster if we directly read only the tags with "NMREDATA" in the tag name... is it possible?
 
-if (molblock.is2D) { // test if the mol is 2d (see the nmredata/wiki page..??)
-    structures.d2.molblok=molblock;
+// if (molblock.is2D) { // test if the mol is 2d (see the nmredata/wiki page..??)
+//     structures.d2.molblok=molblock;
 
-    structures.d2.label_to_atom_table=make_list_refs_atom_to_NMRlabel(nmredata_tags.assignment);
-    //if the .assignment does not exist, don't complain... it can be created and added !? but the list is empty
-}
+//     structures.d2.label_to_atom_table=make_list_refs_atom_to_NMRlabel(nmredata_tags.assignment);
+//     //if the .assignment does not exist, don't complain... it can be created and added !? but the list is empty
+// }
 
-    if molblock.is3d { 
-      structures.d3.molblok=molblock;
-      structures.d3.label_to_atom_table=make_list_refs_atom_to_NMRlabel(nmredata_tags.assignment);;
-    }
-     all_nmredata_tags=   nmredata_tags.addtags (all_sdf_tags);// fuse all NMReDATA tags found
+//     if molblock.is3d { 
+//       structures.d3.molblok=molblock;
+//       structures.d3.label_to_atom_table=make_list_refs_atom_to_NMRlabel(nmredata_tags.assignment);;
+//     }
+//      all_nmredata_tags=   nmredata_tags.addtags (all_sdf_tags);// fuse all NMReDATA tags found
 
-  //end of loop over structures in a given .sdf file
+//   //end of loop over structures in a given .sdf file
   
-  // to be included in a class "structures" 
-  structures.highlight_on("Ha");// will add the yellow shadow about the atoms Ha...
-  structures.highlight_off("Ha")
+//   // to be included in a class "structures" 
+//   structures.highlight_on("Ha");// will add the yellow shadow about the atoms Ha...
+//   structures.highlight_off("Ha")
 
-  display(structures)//we may have one or two structures
+//   display(structures)//we may have one or two structures
   
-  let nmredata = getNMReDATA(nmredata_tags);
-// create a nmredata class...
-  nmredata.display('all content')// to be developped laters...
+//   let nmredata = getNMReDATA(nmredata_tags);
+// // create a nmredata class...
+//   nmredata.display('all content')// to be developped laters...
 
     
