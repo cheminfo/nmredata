3f71a38e27c64842981bfa8ba09faf52ad978b72
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-04 21:13:37 +0100

    added a semicolum

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -6,7 +6,7 @@ import { readNMRR, readNMRRSync } from '../index';
 describe('NMReDATA reading', () => {
   var nmrRecordWithJcampFromSync = readNMRRSync(resolve('testFiles/generated.zip'));
   var zipBrukerSeveralSDF = readFileSync(resolve('testFiles/menthol_1D_1H_assigned_J.zip'), 'base64');
-  var zipWithJcamp = readFileSync(resolve('testFiles/generated.zip'), 'base64');
+  v<qar zipWithJcamp = readFileSync(resolve('testFiles/generated.zip'), 'base64');
   it('Test nmredata read async', async () => {
     var nr = await readNMRR(zipBrukerSeveralSDF);
     var sdfFileList = nr.getSDFList();
@@ -33,7 +33,7 @@ describe('NMReDATA reading', () => {
   });
   it('nmrRecord to json from async, looking spectrum path', async () => {
     var nmrRecordWithJcampFromASync = await readNMRR(zipWithJcamp);
-    var filenames = nmrRecordWithJcampFromASync.getSpectraList()
+    var filenames = nmrRecordWithJcampFromASync.getSpectraList();
     let json = nmrRecordWithJcampFromASync.toJSON();
     for (let nmr of json.spectra.nmr) {
       expect(filenames).toContain(nmr.jcamp.filename);
