b0bcdfc2a747e225b7a060bb2394cd230c51b58e
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-12 11:34:02 +0100

    fix getPascal function for spin>1/2

--- src/__tests__/testPascal.js
+++ src/__tests__/testPascal.js
@@ -2,8 +2,45 @@ import getPascal from '../getPascal';
 
 describe('getPascal', () => {
   it('singlet', () => {
-    let outdata = getPascal(0, 0.5);
-    // expect(spectrum.data).toBeCloseTo({
+    let mult = 0;// singlet
+    let spin = 0.5;// spin 1/2
+    let outdata = getPascal(mult, spin);
     expect(outdata).toStrictEqual([1]);
+  });
+  it('doublet spin 1/2', () => {
+    let mult = 1;
+    let spin = 0.5;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 1]);
+  });
+  it('triplet spin 1/2', () => {
+    let mult = 2;
+    let spin = 0.5;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 2, 1]);
+  });
+  it('Quartet', () => {
+    let mult = 3;
+    let spin = 0.5;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 3, 3, 1]);
+  });
+  it('couplint to one spin 1', () => {
+    let mult = 1;
+    let spin = 1;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 1, 1]);
+  });
+  it('coupling to two spin 1', () => {
+    let mult = 2;
+    let spin = 1;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 2, 3, 2, 1]);
+  });
+  it('coupling to three spin 1', () => {
+    let mult = 3;
+    let spin = 1;
+    let outdata = getPascal(mult, spin);
+    expect(outdata).toStrictEqual([1, 3, 6, 7, 6, 3, 1]);
   });
 });
--- src/getPascal.js
+++ src/getPascal.js
@@ -1,16 +1,22 @@
-function getPascal(n,spin) {
-    if (n===0) return(1);
-    var mult=2*spin+1;
-    var previous_line=[];
-    for (var j=0; j<mult; j++) previous_line.push(1);// initialize with "1 1" or "1 1 1" for spin 1/2 and 1 respectively
-    for (var i=0; i<n-1; i++) {
-    var line=[];
-        for (var j=0; j<mult; j++) {
-        if (j===0) for (var k=0; k<previous_line.length;k++) line.push(previous_line[k]);// copy the line
-        else for (var k=0; k<previous_line.length-1;k++) line[k+j]+=previous_line[k]; // add the previous line
+export default function getPascal(n, spin) {
+  line = [1];
+  if (n === 0) return (line);
+  var mult = 2 * spin + 1;
+  for (var j = 0; j < mult - 1; j++) line.push(1);
+  // complete with "1 1" or "1 1 1" for spin 1/2 and 1 respectively
+  var previousLine = line;
+  for (var i = 0; i < (n - 1); i++) {
+    var line = [];
+    for (j = 0; j < mult; j++) {
+      if (j === 0) { 
+        for (var k = 0; k < previousLine.length; k++) line.push(previousLine[k]);
+      }// copy the line
+      else {
+        for ( k = 0; k < previousLine.length - 1; k++) line[k + j] += previousLine[k]; // add the previous line
         line.push(1); // complete the line
-        } 
-    previous_line=line;
+      }
     }
-    return line;
+    previousLine = line;
+  }
+  return line;
 }
