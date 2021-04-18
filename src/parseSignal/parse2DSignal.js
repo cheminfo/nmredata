3e7e4c12854e44292b5a83e86a632bac5c8a56a5
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-04-18 03:01:18 -0500

    fix: export 2D signal as an object

--- src/parseSignal/parse2DSignal.js
+++ src/parseSignal/parse2DSignal.js
@@ -1,8 +1,6 @@
-import { toObject } from '../converter/util/toObject';
-
 import { getCoupling } from './util/getCoupling';
 
-const axisInOrder = ['xLabel', 'yLabel'];
+const axisInOrder = ['x', 'y'];
 
 export function parse2DSignal(content) {
   content = content.replace(/ /g, '');
@@ -22,16 +20,13 @@ export function parse2DSignal(content) {
 function getSignalWithDelta(data) {
   let { value } = getKeyAndValue(data);
 
-  let signal = [];
+  let signal = { delta: {} };
   let correlation = value.split('/');
   for (let j = 0; j < correlation.length; j++) {
     let label = correlation[j].replace(/[(|)]/g, '').split(',');
-    signal.push({
-      key: axisInOrder[j],
-      value: Array.isArray(label) ? label : [label],
-    });
+    signal.delta[axisInOrder[j]] = Array.isArray(label) ? label : [label];
   }
-  return toObject(signal);
+  return signal;
 }
 
 function getKeyAndValue(data) {
@@ -73,28 +68,3 @@ function chooseKey(key) {
       return key;
   }
 }
-
-// for (let i = 0; i < signals.length; i++) {
-//   if (signals[i].startsWith(';')) continue; // avoid the comments on the record
-//   let signal = {};
-//   let indexComment = signals[i].indexOf(';');
-//   if (indexComment > -1) signals[i] = signals[i].substring(0, indexComment);
-//   signals[i] = signals[i].replace(/ /g, '');
-//   signals[i] = signals[i].replace(/,([0-9])/g, ':$1');
-//   let data = signals[i].split(',');
-
-// data.forEach((d) => {
-//   if (d.toLowerCase().match('larmor')) {
-//     spectrum.frequency = d.toLowerCase().replace('larmor=', '');
-//   } else if (d.toLowerCase().match('cortype')) {
-//     spectrum.experiment = d.toUpperCase().replace(/CORTYPE=/s, '');
-//   } else if (d.toLowerCase().match('spectrum_location')) {
-//     spectrum.spectraLocation = d
-//       .toLowerCase()
-//       .replace('spectrum_location=', '');
-//   } else {
-//     signal.pubAssignment = d;
-//   }
-// });
-// if (Object.keys(signal).length > 0) zone.signal.push(signal);
-// }
