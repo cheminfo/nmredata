679f6472353b0d46c33f9eeb4050e55e859e06e1
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-04-19 16:37:27 -0500

    feat: toJSON without create a NmrRecord instance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { readNmrRecord } from './reader/readNmrRecord';
-export { readNmrRecordSync } from './reader/readNmrRecordSync';
+export { parseSDF } from './parser/parseSDF';
+// export { readNmrRecordSync } from './reader/readNmrRecordSync';
 export { nmriumToNmrRecord } from './converter/nmriumToNmrRecord';
+export { NmrRecord } from './NmrRecord';
