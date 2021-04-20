905cb3109bf1e210ee799fb68eaec27acb6bde12
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-04-20 15:31:41 -0500

    feat: remove sync reading of nmrRecord

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 export { readNmrRecord } from './reader/readNmrRecord';
 export { parseSDF } from './parser/parseSDF';
-// export { readNmrRecordSync } from './reader/readNmrRecordSync';
-export { nmriumToNmrRecord } from './converter/nmriumToNmrRecord';
 export { NmrRecord } from './NmrRecord';
