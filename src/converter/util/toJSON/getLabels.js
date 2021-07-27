9e68d4f5e8b126d06735c96807975a4f6b0e12b4
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-07-27 11:16:13 +0200

    fix: avoid throw if there is not assignment

--- src/converter/util/toJSON/getLabels.js
+++ src/converter/util/toJSON/getLabels.js
@@ -1,6 +1,6 @@
 export function getLabels(content) {
   let labels = {};
-  content.data.forEach((assignment) => {
+  content?.data.forEach((assignment) => {
     let { label, atoms, shift } = assignment.value;
     if (!labels[label]) {
       labels[label] = [];
