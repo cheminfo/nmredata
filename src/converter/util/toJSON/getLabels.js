e4146d055e8c17b75bb7177cdac766f99a6f7c67
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-07-27 11:35:46 +0200

    fix: wrong use of optional chaining operator

--- src/converter/util/toJSON/getLabels.js
+++ src/converter/util/toJSON/getLabels.js
@@ -1,11 +1,11 @@
-export function getLabels(content) {
+export function getLabels(content = {}) {
   let labels = {};
-  content?.data.forEach((assignment) => {
+  for (const assignment of content.data || []) {
     let { label, atoms, shift } = assignment.value;
     if (!labels[label]) {
       labels[label] = [];
     }
     labels[label] = { shift, atoms };
-  });
+  }
   return labels;
 }
