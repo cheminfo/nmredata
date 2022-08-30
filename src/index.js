a6b29d146bbfa2364d7e1cfb6c7b0a85ec47c00b
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-30 04:34:06 -0500

    feat: export getSDF function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { readNmrRecord } from './reader/readNmrRecord';
 export { parseSDF } from './parser/parseSDF';
+export { getSDF } from './util/getSDF';
 export { NmrRecord } from './NmrRecord';
